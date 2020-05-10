d4df0998b1d30cebca23077d5a2b22c6383f6959
Ivan Kerezov <ivan.kerezov@tis.biz>
2020-05-10 11:37:08 +0200

    second

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
  resource "aws_instance" "pdfprint_tfcloud" {
      count = 1
      instance_type           = "r5.xlarge"
-     ami                     = "ami-0789b16020b7c0ac9"
+     ami                     = "ami-0a66e857c49223ad4"
      key_name                = "dev"
      vpc_security_group_ids  = ["sg-075152b4567538a03"]
      subnet_id               = "subnet-0d5930db6c19999ef"
